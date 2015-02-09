80cb88c289481fcb3ac31ea8bf48bd9106d84696
Chupak Daniel <chupakd@gmail.com>
2015-02-09 18:14:23 +0100

    4 teams

--- monequipe.py
+++ monequipe.py
@@ -12,14 +12,18 @@ from td1 import *
 
 team1=SoccerTeam("team1")
 team2=SoccerTeam("team2")
+team3=SoccerTeam("team3")
+team4=SoccerTeam("team4")
 team1.add_player(SoccerPlayer("t1j1",JoueurFonceur()))
 team2.add_player(SoccerPlayer("t2j1",JoueurFonceur()))
 team1.add_player(SoccerPlayer("t1j2",JoueurFonceur()))
 team2.add_player(SoccerPlayer("t2j2",JoueurFonceur()))
-team3.add_player(SoocerPlayer("t4j1",JoueurFonceur()))
-team3.add_player(SoccerPlayer("t4j2",JoueurFonceur()))
+team3.add_player(SoocerPlayer("t3j1",JoueurFonceur()))
+team3.add_player(SoccerPlayer("t3j2",JoueurFonceur()))
+team4.add_player(SoocerPlayer("t4j1",JoueurFonceur()))
+team4.add_player(SoccerPlayer("t4j2",JoueurFonceur()))
 
 
 
 teams=[team1,team2, team3, team4]
-name="Mon Club"
+name="MaccabiTLV"
