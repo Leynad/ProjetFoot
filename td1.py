ae6a59e8634c2404389c6b0a3a34dca959fe4995
Chupak Daniel <chupakd@gmail.com>
2015-02-11 15:07:06 +0100

    Plein de changements...

--- td1.py
+++ td1.py
@@ -9,13 +9,13 @@ from soccersimulator import Vector2D, SoccerAction, SoccerStrategy, pyglet
 from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam 
 from soccersimulator import PygletObserver,ConsoleListener,LogListener
 from soccersimulator import PLAYER_RADIUS, BALL_RADIUS
-import need
+import need, random, math
 
 #######################################################
 #RANDOM
 ######################################################
 
-class RandomStrategy(SoccerStrategy):
+class Random(SoccerStrategy):
     def __init__(self):
         self.name="Random"
     def start_battle(self,state):
@@ -28,9 +28,9 @@ class RandomStrategy(SoccerStrategy):
         action=SoccerAction(acceleration, shoot)
         return action
     def copy(self):
-        return RandomStrategy()
+        return Random()
     def create_strategy(self):
-        return RandomStrategy()
+        return Random()
         
 ########################################################
 #joueurFonceur
@@ -82,26 +82,44 @@ class JoueurFonceurTir(SoccerStrategy):
         return JoueurFonceur()    
 
 #################################################
-#Class de stratégie de base : Aller vers un point
+#Class de stratégie de base : Se déplace vers un point
 ################################################
 
 class AllerVersPoint(SoccerStrategy):
-    def __init__(self, destination):
+    def __init__(self, zone):
         self.name="Random"
-        self.destination = destination
+        self.zone = zone
     def start_battle(self,state):
         pass
     def finish_battle(self,won):
         pass
     def compute_strategy(self,state,player,teamid):
-        self.destination #Créer Vector2D au bon endroit strat = AllerVersPoint(Cevtor2D(**,**))
-        acceleration = self.destination - player.position
+        self.zone #Créer Vector2D au bon endroit strat = AllerVersPoint(Vector2D(**,**))
+        acceleration = self.zone - player.position
         shoot = Vector2D(0,0)
         return SoccerAction(acceleration, shoot)
     def copy(self):
-        return AllerVersPoint(self.destination)
+        return AllerVersPoint(self.zone)
     def create_strategy(self):
-        return AllerVersPoint(self.destination)
+        return AllerVersPoint(self.zone)
+        
+################################        
+#Anda a la PELOTA carajo !
+###############################
+
+class AllerVersPelota(SoccerStrategy):
+    def __init__(self):
+        self.name="AllerVersPelota"
+    def start_battle(self,state):
+        pass
+    def finish_battle(self,won):
+        pass
+    def compute_strategy(self,state,player,teamid):
+        shoot = Vector2D()
+        pos = state.ball.position-player.position
+        return SoccerAction(pos,shoot)
+    def copy(self):
+        return AllerVersPelota()
         
 ######################################
 #Tir
@@ -117,14 +135,14 @@ class AllerVersPoint(SoccerStrategy):
 
 class DefStrategy(SoccerStrategy):
     def __init__(self):
-        self.name="Random"
+        self.name="Def"
     def start_battle(self,state):
         pass
     def finish_battle(self,won):
         pass
     def compute_strategy(self,state,player,teamid):
         d = state.ball.position - player.position
-        if d.norm > 25:
+        if d.norm > 10:
             acceleration = state.ball.position + state.get_goal_center(teamid) - player.position - player.position
             
         else:
@@ -137,15 +155,14 @@ class DefStrategy(SoccerStrategy):
     def copy(self):
         return DefStrategy()
     def create_strategy(self):
-        return DefStrategy()        
+        return DefStrategy()    
         
 ########################################
-#compose strat
+#Compose strat
 ###################################
 
 class ComposeStrategy(SoccerStrategy):
     def __init__(self, strategy1, strategy2):
-        self.name="Random"
         self.strategy1 = strategy1
         self.strategy2 = strategy2
     def start_battle(self,state):
@@ -153,57 +170,93 @@ class ComposeStrategy(SoccerStrategy):
     def finish_battle(self,won):
         pass
     def compute_strategy(self,state,player,teamid):
-        return (state.strategy1, state.strategy2.compute)
-    def copy(self):
-        return ComposeStrategy(self.strategy1, slef.strategy2)
+        strategy1 = self.strategy1.compute_strategy(state,player,teamid)
+        strategy2 = self.strategy2.compute_strategy(state,player,teamid)
+        return SoccerAction(strategy1,strategy2)
     def create_strategy(self):
         return ComposeStrategy()
-        
-        
-        
-        
-###
+          
+#############################
 #Test
-###
+########################
 
 class TestStrat(SoccerStrategy):
     def __init__(self):
-        self.name="Fonceur"
+        self.name="Testeur"
     def start_battle(self,state):
         pass
     def finish_battle(self,won):
         pass
     def compute_strategy(self,state,player,teamid):
-        pos = state.ball.position-player.position
+        d = state.ball.position - player.position
+        if d.norm > 15:
+            acceleration = Vector2D((1.2/5)*GAME_WIDTH,state.ball.position.y)-player.position
+            
+        else:
+            acceleration = state.ball.position - player.position
+
         shoot = Vector2D(0,0)
         if (PLAYER_RADIUS+BALL_RADIUS)>=(state.ball.position.distance(player.position)):
-            teamadverse=2
-            if teamid==2:
-                teamadverse=1    
             shoot= (state.get_goal_center(need.teamAdverse(teamid))-player.position)
+        return SoccerAction(acceleration, shoot)
             
-        return SoccerAction(pos,shoot)
     def copy(self):
         return TestStrat()
     def create_strategy(self):
         return TestStrat()
         
-###
+#######################
 # Defenseur qui tir la balle sur un coté
-###        
+#######################        
         
 class TirAngle(SoccerStrategy):
     def __init__(self):
-        pass
+        self.name="Goal"
     def start_battle(self,state):
         pass
     def finish_battle(self,won):
         pass
     def compute_strategy(self,state,player,teamid):
-        shoot = Vector2D.create_polar(player.angle + random.random()*2-1, 1)
-        return SoccerAction(Vector2D(0,0), shoot)
+        shoot = Vector2D.create_polar(player.angle + 2.5, 100)
+        return SoccerAction(Vector2D(5,15), shoot)
     def copy(self):
         return TirAngle()
     def create_strategy(self):
         return TirAngle()
+        
+###
+#Goal
+###
 
+class Goal(SoccerStrategy):
+    def __init__(self):
+        self.name="Goal"
+    def start_battle(self,state):
+        pass
+    def finish_battle(self,won):
+        pass
+    def compute_strategy(self,state,player,teamid):
+            a=(state.ball.position+state.get_goal_center((teamid)))
+            a.x=a.x/2
+            a.y=a.y/2
+            a=a-player.position
+            shoot = create_polar(6,(teamid-1.5)*20)
+            return SoccerAction(a,shoot)#Soit il tir a 10 soit a -10 en fonction de son équipe  
+    def create_strategy(self):
+        return Goal()   
+        
+#########
+#Wait
+########
+
+class Wait(SoccerStrategy):
+    def __init__(self):
+        self.name="Wait"      
+    def start_battle(self,state):
+        pass
+    def finish_battle(self,won):
+        pass
+    def compute_strategy(self,state,player,teamid):
+        return SoccerAction(Vector2D(0,0),Vector2D(0,0))
+    def copy(self):
+        return Wait()
